76754f84144b7ba198c248a41a78094e78ae0627
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2024-11-05 22:27:55 +0700

    v3

--- src/config.js
+++ src/config.js
@@ -3,5 +3,7 @@ export default {
 	// URL: 'https://knowledge-graph.up.railway.app',
 	API_URL: 'http://localhost:8000/api/',
 	URL: 'http://localhost:8000',
-	DOMAIN_CLIENT: 'http://localhost:8080',
+	DOMAIN_CLIENT: 'http://localhost:8080/',
+	// DOMAIN_CLIENT: 'https://advanced-knowledge.vercel.app/',
+	// DOMAIN_CLIENT: 'http://20.2.90.6/',
 }
